7c3d172aeb5bfb8220c095ca6f5cba1e11b1662d
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-07-13 17:09:17 +0000

    error with duplicate broswer import modules

--- first-angular-project/src/app/app.component.ts
+++ first-angular-project/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CollapseWithStickyHeaderComponent } from './collapse-with-sticky-header/collapse-with-sticky-header.component';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CollapseWithStickyHeaderComponent],
+  imports: [RouterOutlet,CollapseWithStickyHeaderComponent,NzFlexModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- first-angular-project/src/app/collapse-with-sticky-header/collapse-with-sticky-header.component.ts
+++ first-angular-project/src/app/collapse-with-sticky-header/collapse-with-sticky-header.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+
+
 
 @Component({
   selector: 'app-collapse-with-sticky-header',
   standalone: true,
-  imports: [],
+  imports: [NzCollapseModule, BrowserAnimationsModule ],
   templateUrl: './collapse-with-sticky-header.component.html',
   styleUrl: './collapse-with-sticky-header.component.css'
 })
 export class CollapseWithStickyHeaderComponent {
+
+  panels = [
+    {
+      active: true,
+      name: 'This is panel header 1',
+      arrow: true
+    },
+    {
+      active: false,
+      arrow: false,
+      name: 'This is panel header 2'
+    }
+  ];
 
 }
