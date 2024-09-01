995afdfba1c2ddd7b724019580eabab0d664d499
Ratan Mourya <mourya.tutor@gmail.com>
2024-09-01 10:50:32 +0530

    Avoiding zone polution

--- change-detection-starting-project/src/app/counter/counter.component.ts
+++ change-detection-starting-project/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -9,8 +9,24 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
   count = signal(0);
+  private zone = inject(NgZone);
+
+  ngOnInit() {
+    
+    setTimeout(() => {
+      this.count.set(0);
+    },4000);
+
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('Timer Expired');
+      },6000);
+    });
+
+  }
+
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
